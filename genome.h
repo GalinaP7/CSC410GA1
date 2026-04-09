0e80fbdc7388e7d3c7fdb5f162a4dda666a762e7
pokitkog <pokitkog@berea.edu>
2026-04-09 18:37:21 -0400

    GA1 genome implementation with unit tests - reverted; changes for GA2 were incidentally added to the same repo

--- genome.h
+++ genome.h
@@ -21,20 +21,10 @@ public:
     void set_blue(int index, int value); 
     int get_blue(int index); 
     void print();
-    void set_mRate(double val); //sets the mRate equal to val
-    double get_mRate(); //returns the current mRate value
-    void mutate_gene(int index); //for each colour in the Pixel at index, generates random num 0-1
-    void mutate(); //calls mutate_gene on every Pixel in the genes array
-    double calculate_gene_fitness(int index, Pixel targetPixel); //returns average percent difference between genes[index] and targetPixel
-    double calculate_overall_fitness(Pixel* target, int nPixels); //returns average error across all pixels between genome and target array
-    void set_pixel(int index, Pixel newPixel); //sets Pixel at index to newPixel, ignores invalid index/RGB values
-    Pixel get_pixel(int index); //returns the Pixel at index
-
 
 private:
     Pixel* genes; // pointer to the array of Pixels
     int nGenes;   // stores the num of Pixels in array
-    double mRate; //to store a number between 0 and 1 for the mutation rate
 };
 
 #endif
